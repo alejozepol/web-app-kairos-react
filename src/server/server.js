6260d10eae70b23441648bc688ecb5bfdf69f8ef
Alejandro López Ramírez <alejozepol@gmail.com>
2020-08-11 18:31:31 -0500

    server production

--- src/server/server.js
+++ src/server/server.js
@@ -7,34 +7,27 @@ import main from './routes/main';
 
 dovenv.config();
 
-const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-if (ENV === 'production') {
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const webpackConfig = require('../../webpack.config');
-  const compiler = webpack(webpackConfig);
-  const serverConfig = {
-    contentBase: `http://localhost:${PORT}`,
-    port: PORT,
-    publicPath: webpackConfig.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    stats: { colors: true },
-  };
-  app.use(webpackDevMiddleware(compiler, serverConfig));
-  app.use(webpackHotMiddleware(compiler));
-  console.log('Ejecucion en entorno de produccion');
-} else {
-  app.use(helmet());
-  app.use(helmet.permittedCrossDomainPolicies());
-  app.disable('x-powered-by');
-  console.log('Ejecucion en entorno de desarrollo');
-}
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackConfig = require('../../webpack.config');
+
+const compiler = webpack(webpackConfig);
+const serverConfig = {
+  contentBase: `http://localhost:${PORT}`,
+  port: PORT,
+  publicPath: webpackConfig.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: { colors: true },
+};
+app.use(webpackDevMiddleware(compiler, serverConfig));
+app.use(webpackHotMiddleware(compiler));
+
 
 app.get('*', main);
 app.listen(PORT, (err) => {
