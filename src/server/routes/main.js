ea3d4bef9f0131486376dfc09406df434bfe14bc
Alejandro Lopez Ramirez <alejozepol@gmail.com>
2021-02-23 14:32:54 -0500

    fix get api

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -11,72 +11,73 @@ import reducer from '../../frontend/redux/reducers';
 import render from '../render';
 import initialState from '../../frontend/redux/initialState';
 
-const API = 'https://api.kairosshop.xyz/api';
+const API = 'https://www.api-kairos.extrasistemas.com/api';
 
 async function getCategories() {
-  return fetch(`${API}/categories`)
-    .then((response) => response.json())
-    .then(({ body, error }) => {
-      if (error) {
-        return console.error(error);
-      }
-      return body;
-    })
-    .then((data) => {
-      initialState.categories = data;
-    })
-    .catch((error) => console.error(error));
+    return fetch(`${API}/categories`)
+        .then((response) => response.json())
+        .then(({ body, error }) => {
+            if (error) {
+                return console.error(error);
+            }
+            return body;
+        })
+        .then((data) => {
+            initialState.categories = data;
+        })
+        .catch((error) => console.error(error));
 
 }
 
 async function getProductsOfCategory(categoryId, title) {
-  return fetch(`${API}/products/?categoryId=${categoryId}`)
-    .then((response) => response.json())
-    .then(({ body, error }) => {
-      if (error) {
-        return console.error(error);
-      }
-      return body;
-    })
-    .then((data) => {
-      const object = {
-        categoryId,
-        title,
-        products: [...data],
-      };
-      initialState.productsOfCategories = [...initialState.productsOfCategories, object];
-    })
-    .catch((error) => console.error(error));
+    return fetch(`${API}/products/?categoryId=${categoryId}`)
+        .then((response) => response.json())
+        .then(({ body, error }) => {
+            if (error) {
+                return console.error(error);
+            }
+            return body;
+        })
+        .then((data) => {
+            const object = {
+                categoryId,
+                title,
+                products: [...data],
+            };
+            initialState.productsOfCategories = [...initialState.productsOfCategories, object];
+        })
+        .catch((error) => console.error(error));
 }
 
 async function init(req, res, next) {
-  try {
-    const store = createStore(reducer, initialState);
-    const html = renderToString(
-      <Provider store={store}>
-        <StaticRouter
-          location={req.url}
-          context={{}}
-        >
-          <Layout location={req.url}>
-            {renderRoutes(Routes)}
-          </Layout>
-        </StaticRouter>
-      </Provider>,
-    );
-    const preloadedState = store.getState();
-    res.send(render(html, preloadedState));
-  } catch (error) {
-    next(error);
-  }
-}
+    try {
+        const store = createStore(reducer, initialState);
+        const html = renderToString( < Provider store = { store } >
+            <
+            StaticRouter location = { req.url }
+            context = {
+                {}
+            } >
+            <
+            Layout location = { req.url } > { ' ' } { renderRoutes(Routes) } { ' ' }
 
-const main = async (req, res, next) => {
-  await getCategories()
-    .then(() => { initialState.productsOfCategories = []; })
-    .catch((e) => console.log(e));
-  await getProductsOfCategory(initialState.categories[0].id, initialState.categories[0].title);
-  await init(req, res, next);
-};
+            <
+            /Layout> <
+            /StaticRouter> <
+            /Provider>);
+            const preloadedState = store.getState(); res.send(render(html, preloadedState));
+        }
+        catch (error) {
+            next(error);
+        }
+    }
 
-export default main;
+    const main = async(req, res, next) => {
+        await getCategories()
+            .then(() => { initialState.productsOfCategories = []; })
+            .catch((e) => console.log(e));
+        await getProductsOfCategory(initialState.categories[0].id, initialState.categories[0].title);
+        await init(req, res, next);
+    };
+
+    export default main;
